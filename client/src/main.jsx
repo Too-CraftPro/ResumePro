7c58da4772dcee5630b8e9e608105f6cfeaa038d
Too-CraftPro <toolcraftpro@gmail.com>
2025-07-07 13:34:27 +0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
-import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context/AuthContext'; // <-- This is the corrected line
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-        <Toaster position="top-right" />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
